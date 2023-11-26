fb9a465331e4578393576cd1ac80267959e21644
Keshav182 <109676503+Keshav182@users.noreply.github.com>
2023-11-26 17:03:23 +0530

    PID.h

--- PID.h
+++ PID.h
@@ -1,33 +1,24 @@
 #ifndef PID_H
 #define PID_H
 
+#include <vector>
+
 class PID {
 public:
-  
-    //   Errors
-    double p_error;
-    double i_error;
-    double d_error;
 
-    // Coefficients    
-    double Kp;
-    double Ki;
-    double Kd;
+  double Kp_;
+  double Ki_;
+  double Kd_;
 
-    // Constructor
-    PID();
+  std::vector<double> error_vector_;
+  PID();
+  virtual ~PID();
 
-    // Destructor.    
-    virtual ~PID();
+  void Init(double Kp, double Ki, double Kd);
 
-    // Initialize PID.
-    void Init(double Kp, double Ki, double Kd);
+  std::vector<double> UpdateError(double cte);
 
-    // Update the PID error variables given CTE.
-    void UpdateError(double cte);
-
-    // Calculate the total PID error.
-    double TotalError();
+  double TotalError();
 };
 
 #endif
